10a52263c08a7841b2e1fb6e531fbe1b6966f872
beetkar <beetk4r@gmail.com>
2025-09-26 17:15:44 +0400

    Delete functions/api directory

--- functions/api/_middleware.ts
+++
@@ -1,7 +0,0 @@
-// functions/_middleware.ts
-export const onRequest: PagesFunction = async (ctx) => {
-  const resp = await ctx.next();
-  const h = new Headers(resp.headers);
-  h.set('access-control-allow-origin', new URL(ctx.request.url).origin);
-  return new Response(resp.body, { status: resp.status, headers: h });
-};
